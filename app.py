0f1962d15437c09a58cdc0cdcdadff0d96726819
Tahir MAT <matahir333@gmail.com>
2023-05-01 17:55:59 +0200

    .

--- app.py
+++ app.py
@@ -1,2 +1,32 @@
-import streamlit 
-import pandas
+import itertools
+import requests
+
+# importing geopy library
+from geopy.geocoders import Nominatim
+
+# calling the Nominatim tool
+loc = Nominatim(user_agent="GetLoc")
+
+# entering the location name
+
+
+
+
+addresses = [
+    "Empire State Building, New York City",
+    "Statue of Liberty, New York City",
+    "Central Park, New York City",
+    "One World Trade Center, New York City",
+    "Brooklyn Bridge, New York City"
+]
+
+lst = []
+
+for addr in addresses:
+    getLoc = loc.geocode(addr)
+    dct = {"Addr":addr, "Lat": getLoc.latitude, "Lon": getLoc.longitude}
+    lst.append(dct)
+
+
+
+print(lst[0]["Lat"])
